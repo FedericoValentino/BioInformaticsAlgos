4b294f093b928641b61a73b53865a84b2a9eb349
FedericoValentino <10679810@polimi.it>
2022-07-18 21:23:33 +0200

    qualsiasi cosa

---
+++ BurrowsWheeler/burrow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by valef on 18/07/2022.
+//
+
+#include "burrow.h"
---
+++ BurrowsWheeler/burrow.h
@@ -0,0 +1,14 @@
+//
+// Created by valef on 18/07/2022.
+//
+
+#ifndef BIOINFORMATICS_BURROW_H
+#define BIOINFORMATICS_BURROW_H
+
+
+class burrow {
+
+};
+
+
+#endif //BIOINFORMATICS_BURROW_H
